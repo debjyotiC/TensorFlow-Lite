e393aa6f414c3483994a07c6c1872290dcdb1149
debjyotiC <djbabai.debjyoti@gmail.com>
2020-08-07 20:11:38 +0530

    updated the Keras model

--- KerasModel.py
+++ KerasModel.py
@@ -4,11 +4,9 @@ import tensorflow as tf
 celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-l0 = tf.keras.layers.Dense(units=4, input_shape=[1], activation='relu')
-l1 = tf.keras.layers.Dense(units=4, activation='relu')
-l2 = tf.keras.layers.Dense(units=1)
-
-model = tf.keras.Sequential([l0, l1, l2])
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=4, input_shape=[1]),
+                             tf.keras.layers.Dense(units=2),
+                             tf.keras.layers.Dense(units=1)])
 
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
 history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
